cf7831466fdd8e2689ed223ee0fd2c33c15c82d3
RebellWrynn <egzinivan@gmail.com>
2024-09-10 08:21:31 +0500

    do some in models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class employees(models.Model):
+    firstname = models.CharField(max_leigth=30)
+    lastname = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+
+class requests(models.Model):
+    REQUEST_TYPE = {
+        "S": "Обслуживание",
+        "I": "Установка оборудования",
+        "O": "Другой  вариант",
+        
+
+    }
+    them = models.Charfield(max_leigth=30)
+    requesttype = models.Charfield(max_leigth=30,choices = REQUEST_TYPE)
+    priority = models.Charfield(max_leigth=30)
+    description = models.Charfield(max_leigth=255)
+    date = models.DateField()
+    responsibleemployee = models.Charfield(max_leigth=30)
