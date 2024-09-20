ec571236979f97db70712c7cacff8badb9965329
RebellWrynn <egzinivan@gmail.com>
2024-09-21 00:45:43 +0500

    remove sql

--- localrequest/settings.py
+++ localrequest/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'localrequest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "RequestSite",
-        "USER": "postgres",
-        "PASSWORD": "lj;lbr",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "RequestSite",
+#         "USER": "postgres",
+#         "PASSWORD": "lj;lbr",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
